33aec16653a3e432380288deb7411c1bb3eb1e28
Fabrício Godoy <skarllot@gmail.com>
2024-02-25 23:45:53 -0300

    test: ignore attributes coverage

--- src/EnumUtilities.Abstractions/EnumGeneratorAttribute.cs
+++ src/EnumUtilities.Abstractions/EnumGeneratorAttribute.cs
@@ -1,4 +1,7 @@
-﻿namespace Raiqub.Generators.EnumUtilities;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Raiqub.Generators.EnumUtilities;
 
 [AttributeUsage(AttributeTargets.Enum)]
+[ExcludeFromCodeCoverage]
 public sealed class EnumGeneratorAttribute : Attribute;
--- src/EnumUtilities.Abstractions/JsonConverterGeneratorAttribute.cs
+++ src/EnumUtilities.Abstractions/JsonConverterGeneratorAttribute.cs
@@ -1,6 +1,9 @@
-﻿namespace Raiqub.Generators.EnumUtilities;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Raiqub.Generators.EnumUtilities;
 
 [AttributeUsage(AttributeTargets.Enum)]
+[ExcludeFromCodeCoverage]
 public sealed class JsonConverterGeneratorAttribute : Attribute
 {
     public bool AllowIntegerValues { get; set; } = true;
