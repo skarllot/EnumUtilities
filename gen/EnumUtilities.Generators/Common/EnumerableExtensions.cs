6903c48311dd13be6215c659b18d1842b8420cb8
Fabrício Godoy <skarllot@gmail.com>
2024-10-06 13:36:37 -0300

    build(deps): update multiple packages
    - Update dependency polyfill to v6
    - Update dependency nerdbank.gitversioning to 3.6.143
    - Update dependency dotnet.reproduciblebuilds to 1.2.25

--- gen/EnumUtilities.Generators/Common/EnumerableExtensions.cs
+++ gen/EnumUtilities.Generators/Common/EnumerableExtensions.cs
@@ -2,19 +2,6 @@
 
 public static class EnumerableExtensions
 {
-    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(
-        this IEnumerable<TSource> source,
-        Func<TSource, TKey> keySelector,
-        IEqualityComparer<TKey>? comparer = null)
-    {
-        var hashSet = new HashSet<TKey>(comparer);
-        foreach (var item in source)
-        {
-            if (hashSet.Add(keySelector(item)))
-                yield return item;
-        }
-    }
-
     public static string JoinToString(this byte[] source)
     {
         Span<char> result = stackalloc char[source.Sum(GetStringLength) + (2 * (source.Length - 1))];
