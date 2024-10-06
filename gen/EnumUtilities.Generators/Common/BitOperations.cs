a3d0aa532c0493273bc9ea3f2c8244877c88d462
Fabrício Godoy <skarllot@gmail.com>
2024-10-06 10:42:57 -0300

    chore: Remove unused bit operations

--- gen/EnumUtilities.Generators/Common/BitOperations.cs
+++ gen/EnumUtilities.Generators/Common/BitOperations.cs
@@ -5,14 +5,6 @@ namespace Raiqub.Generators.EnumUtilities.Common;
 
 public static class BitOperations
 {
-    private static ReadOnlySpan<byte> TrailingZeroCountDeBruijn => new byte[32]
-    {
-        00, 01, 28, 02, 29, 14, 24, 03,
-        30, 22, 20, 15, 25, 17, 04, 08,
-        31, 27, 13, 23, 21, 19, 16, 07,
-        26, 12, 18, 06, 11, 05, 10, 09
-    };
-
     private static ReadOnlySpan<byte> Log2DeBruijn => new byte[32]
     {
         00, 09, 01, 10, 13, 21, 02, 29,
@@ -43,36 +35,6 @@ public static class BitOperations
         }
 
         return LeadingZeroCount(hi);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int TrailingZeroCount(uint value)
-    {
-        // Unguarded fallback contract is 0->0, BSF contract is 0->undefined
-        if (value == 0)
-        {
-            return 32;
-        }
-
-        // uint.MaxValue >> 27 is always in range [0 - 31] so we use Unsafe.AddByteOffset to avoid bounds check
-        return Unsafe.AddByteOffset(
-            // Using deBruijn sequence, k=2, n=5 (2^5=32) : 0b_0000_0111_0111_1100_1011_0101_0011_0001u
-            ref MemoryMarshal.GetReference(TrailingZeroCountDeBruijn),
-            // uint|long -> IntPtr cast on 32-bit platforms does expensive overflow checks not needed here
-            (IntPtr)(int)(((value & (uint)-(int)value) * 0x077CB531u) >> 27)); // Multi-cast mitigates redundant conv.u8
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int TrailingZeroCount(ulong value)
-    {
-        uint lo = (uint)value;
-
-        if (lo == 0)
-        {
-            return 32 + TrailingZeroCount((uint)(value >> 32));
-        }
-
-        return TrailingZeroCount(lo);
     }
 
     private static int Log2SoftwareFallback(uint value)
