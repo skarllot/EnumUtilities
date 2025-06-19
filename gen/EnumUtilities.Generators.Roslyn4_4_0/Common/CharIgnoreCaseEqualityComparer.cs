09182f57e28a6c74e8d0a38af1a24d5daaa12131
Fabrício Godoy <skarllot@gmail.com>
2025-06-19 16:54:46 -0300

    fix: Group first-letters correctly for parsing enum value #357

---
+++ gen/EnumUtilities.Generators.Roslyn4_4_0/Common/CharIgnoreCaseEqualityComparer.cs
@@ -0,0 +1,13 @@
+﻿namespace Raiqub.Generators.EnumUtilities.Common;
+
+public sealed class CharIgnoreCaseEqualityComparer : IEqualityComparer<char>
+{
+    public static CharIgnoreCaseEqualityComparer Instance { get; } = new();
+
+    public bool Equals(char x, char y) => NormalizeChar(x) == NormalizeChar(y);
+
+    public int GetHashCode(char obj) => NormalizeChar(obj).GetHashCode();
+
+    private static char NormalizeChar(char ch) =>
+        char.IsSurrogate(ch) || char.IsControl(ch) ? ch : char.ToUpperInvariant(ch);
+}
---
+++ tests/EnumUtilities.Generators.IntegrationTests/Models/Bug357.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Raiqub.Generators.EnumUtilities.IntegrationTests.Models;
+
+[EnumGenerator]
+[JsonConverterGenerator]
+[SuppressMessage("Naming", "CA1708:Identifiers should differ by more than case")]
+public enum Bug357
+{
+    Py1,
+    pay,
+    PAY,
+    John,
+    john,
+    JOHN,
+}
