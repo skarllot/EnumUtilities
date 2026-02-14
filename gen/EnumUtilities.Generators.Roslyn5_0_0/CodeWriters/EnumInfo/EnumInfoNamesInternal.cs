a6de1098b5856661c6528e1ea419aef9a565f1c5
Fabrício Godoy <skarllot@gmail.com>
2026-02-14 13:58:14 -0300

    chore: Use Raiqub.Generators.InterpolationCodeWriter.CSharp.Sources

--- gen/EnumUtilities.Generators.Roslyn5_0_0/CodeWriters/EnumInfo/EnumInfoNamesInternal.cs
+++ gen/EnumUtilities.Generators.Roslyn5_0_0/CodeWriters/EnumInfo/EnumInfoNamesInternal.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using Raiqub.Generators.EnumUtilities.Common;
 using Raiqub.Generators.EnumUtilities.Models;
+using Raiqub.Generators.InterpolationCodeWriter;
 
 namespace Raiqub.Generators.EnumUtilities.CodeWriters.EnumInfo;
 
@@ -9,7 +10,7 @@ public static class EnumInfoNamesInternal
     private static readonly Encoding s_utf8Encoding = Encoding.UTF8;
 
     public static void Write(
-        StringBuilder builder,
+        SourceTextWriter writer,
         EnumToGenerate model,
         string className,
         string xmlDocAllRef,
@@ -18,57 +19,57 @@ public static class EnumInfoNamesInternal
         Func<EnumValue, string> keySelector
     )
     {
-        builder.AppendUnixLine(
+        writer.WriteLine(
             $$"""
-                /// <summary>Provides constant values for <see cref="{{model.Name}}" /> {{xmlDocAllRef}}.</summary>
-                public static partial class {{className}}
-                {
-                    /// <summary>Represents the largest possible number of characters produced by {{xmlDocAction}} a <see cref="{{model.Name}}" /> value to string, based on defined members.</summary>
-                    public const int MaxCharsLength = {{model.Values.Max(x => keySelector(x).Length)}};
+            /// <summary>Provides constant values for <see cref="{{model.Name}}" /> {{xmlDocAllRef}}.</summary>
+            public static partial class {{className}}
+            {
+                /// <summary>Represents the largest possible number of characters produced by {{xmlDocAction}} a <see cref="{{model.Name}}" /> value to string, based on defined members.</summary>
+                public const int MaxCharsLength = {{model.Values.Max(x => keySelector(x).Length)}};
             """
         );
 
+        writer.PushIndent();
         foreach (var curr in model.Values)
         {
-            builder.AppendUnixLine(
+            writer.WriteLine(
                 $$"""
 
-                        /// <summary>The string representation of <see cref="{{model.Name}}.{{curr.MemberName}}" /> {{xmlDocItemRef}}.</summary>
-                        public const string {{curr.MemberName}} = {{keySelector(curr).ToQuotedStringLiteral()}};
+                /// <summary>The string representation of <see cref="{{model.Name}}.{{curr.MemberName}}" /> {{xmlDocItemRef}}.</summary>
+                public const string {{curr.MemberName}} = {{keySelector(curr).ToQuotedStringLiteral()}};
                 """
             );
         }
+        writer.PopIndent();
 
         var maxBytesLength = model.Values.Max(x => s_utf8Encoding.GetByteCount(keySelector(x)));
-        builder.AppendUnixLine(
+        writer.WriteLine(
             $$"""
-                }
+            }
 
-                /// <summary>Provides static values for <see cref="{{model.Name}}" /> UTF-8 encoded {{xmlDocAllRef}}.</summary>
-                public static partial class Utf8{{className}}
-                {
-                    /// <summary>Represents the largest possible number of bytes produced by {{xmlDocAction}} a <see cref="{{model.Name}}" /> value to UTF-8 string, based on defined members.</summary>
-                    public const int MaxBytesLength = {{maxBytesLength}};
+            /// <summary>Provides static values for <see cref="{{model.Name}}" /> UTF-8 encoded {{xmlDocAllRef}}.</summary>
+            public static partial class Utf8{{className}}
+            {
+                /// <summary>Represents the largest possible number of bytes produced by {{xmlDocAction}} a <see cref="{{model.Name}}" /> value to UTF-8 string, based on defined members.</summary>
+                public const int MaxBytesLength = {{maxBytesLength}};
             """
         );
 
+        writer.PushIndent();
         foreach (var curr in model.Values)
         {
             var byteCount = s_utf8Encoding.GetByteCount(keySelector(curr));
             var bytes = s_utf8Encoding.GetBytes(keySelector(curr)).JoinToString();
-            builder.AppendUnixLine(
+            writer.WriteLine(
                 $$"""
 
-                        /// <summary>The UTF-8 representation of <see cref="{{model.Name}}.{{curr.MemberName}}" /> {{xmlDocItemRef}}.</summary>
-                        public static ReadOnlySpan<byte> {{curr.MemberName}} => new byte[{{byteCount}}] { {{bytes}} };
+                /// <summary>The UTF-8 representation of <see cref="{{model.Name}}.{{curr.MemberName}}" /> {{xmlDocItemRef}}.</summary>
+                public static ReadOnlySpan<byte> {{curr.MemberName}} => new byte[{{byteCount}}] { {{bytes}} };
                 """
             );
         }
+        writer.PopIndent();
 
-        builder.AppendUnixLine(
-            """
-                }
-            """
-        );
+        writer.WriteLine('}');
     }
 }
