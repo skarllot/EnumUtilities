415077e61ea8992c4437ce65033a70f150715841
Fabrício Godoy <skarllot@gmail.com>
2026-04-04 18:56:40 -0300

    chore: Update to latest version of Raiqub.Generators.InterpolationCodeWriter.CSharp.Sources

--- gen/EnumUtilities.Generators.Roslyn5_0_0/Suppresses.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage(
-    "Design",
-    "MA0008:Add StructLayoutAttribute",
-    Justification = "External source package",
-    Scope = "type",
-    Target = "~T:Raiqub.Generators.InterpolationCodeWriter.TextSegment.Enumerator"
-)]
